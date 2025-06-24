7ad35f0094a0132035d3a0de59aaf800e8027079
Priyanshu Mishra <145151434+CodeNerd42@users.noreply.github.com>
2025-06-24 11:00:08 +0000

    data_types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"Use Strict"; // treat all JS code as newer version
+
+// alert (3+3);   // We are using node.js, not browser, so alert will not work
+
+/* console.log(3
+    +
+    3) // Code Readability shoul be hight
+
+*/
+
+console.log("Priyanshu");
+ 
+let firstName = "Priyanshu";
+let age = 21;
+let isLoggedIn = true;
+let temp = null; // no value
+let undefinedVariable; // variable that is not defined
+// Data Types in JavaScript
+
+// Primitive Data Types
+
+
+// number => 2^53
+// bigInt
+// string = ""
+// boolean = true/false"
+// null = standalone value
+// undefined = variable that is not defined 
+// symbol = unique and immutable value
+
+// objects
+
+// Typeof operator
+console.log(typeof firstName); // string
+console.log(typeof temp);
