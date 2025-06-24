9a57a2e733387dcc8b2623b2c1feac2a5bc0f8bd
Priyanshu Mishra <145151434+CodeNerd42@users.noreply.github.com>
2025-06-24 17:13:23 +0000

    Comparisions in datatypes

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,20 @@
+// console.log(2>=1);
+
+// console.log("2">1);
+// console.log("02" > 1); // true, string is converted to number
+
+
+
+// console.log(null >0);
+// console.log(null >= 0); // true, null is converted to 0
+// console.log(null == 0); // false, null is not equal to 0
+
+
+//  console.log(undefined > 0); // false, undefined is not a number
+// console.log(undefined >= 0); // false, undefined is not a number
+// console.log(undefined == 0); // false, undefined is not equal to 0
+ 
+
+console.log("2"===2);
+console.log("2"==2); // true, string is converted to number
+
