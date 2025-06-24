e81f478404bccdcc74b5aad91942a9347b59d588
Priyanshu Mishra <145151434+CodeNerd42@users.noreply.github.com>
2025-06-24 16:31:49 +0000

    03_conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = "Priyanshu"
+
+// console.log(typeof score); // string concatenation
+// console.log(typeof(score));
+
+// let valueNumber = Number(score);
+
+// console.log(typeof valueNumber); // number
+// console.log(valueNumber); 
+
+// "33"=> 33
+// "33abc"=> NaN
+// true => 1
+// false => 0
+
+// let isLoggedIn = "fdfcvef";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn); // true
+// console.log(typeof booleanIsLoggedIn); // boolean
+
+// "Priyanshu" => true
+// "" => false
+// "0" => true
+// "1" => true
+// "0" => false
+
+let soneNumber = "123";
+let stringNumber = String(soneNumber);
+
+console.log(stringNumber); // "123"
+console.log(typeof stringNumber); // string
